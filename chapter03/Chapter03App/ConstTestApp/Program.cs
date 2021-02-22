e7a40f85512e468cfe678d757ebb770c416c6775
taekyom <tkyoung1014@naver.com>
2021-02-22 17:05:27 +0900

    3장 완료

---
+++ chapter03/Chapter03App/ConstTestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConstTestApp
+{
+    class Program
+    {
+        enum Season
+        {
+            봄=1000,
+            여름=2000,
+            가을=3000,
+            겨울=4000
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("상수테스트");
+            const double Pi = 3.141592;
+            Console.WriteLine($"원주율의 값은 {Pi}");
+            Season mySeason = Season.여름;
+            Console.WriteLine($"지금 계절은 {mySeason}입니다");
+
+            int a = 0;
+            Console.WriteLine($"a는 {a}");
+            int? b = null;
+            Console.WriteLine($"b는 {b}");
+
+        }
+    }
+}
---
+++ chapter03/Chapter03App/IntConversionApp/ConvTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntConversionApp
+{
+    class ConvTest
+    {
+        static void Main(string[] args)
+        {
+            short shMaxVal = short.MaxValue;
+            int inCastVal = 0; //0으로 초기화
+
+            inCastVal = shMaxVal;
+            Console.WriteLine($"short값 변환한 int값은 {inCastVal}");
+
+            inCastVal += 5; //32767+5 = 32772
+            short shCastVal = (short)inCastVal;
+            Console.WriteLine($"int값 변환한 short값은 {shCastVal}"); //오버플로우
+
+            float flVal = 3.141592f; //float값이라고 정의 : f or F 입력
+            inCastVal = (int)flVal;
+            Console.WriteLine($"float값 변환한 int값은 {inCastVal}");
+            double dlVal = inCastVal; //3
+            Console.WriteLine($"int값 변환한 double값은 {dlVal}");
+
+            object obj = 20; // Boxing
+            int inUnboxingVal = (int)obj; //Unboxing
+
+            string strVal = "200";
+            int result = int.Parse(strVal) * 3; //parse : 문자열을 숫자로 형변환
+            Console.WriteLine($"200 * 3 = {result}");
+        }
+    }
+}
