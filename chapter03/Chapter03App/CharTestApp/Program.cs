3f1f4d775a7dedc284a6a030b75ff5baa089dd76
taekyom <tkyoung1014@naver.com>
2021-02-22 15:15:40 +0900

    3장 중간 커밋

---
+++ chapter03/Chapter03App/CharTestApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //문자
+            char ch1 = 't', ch2 = '\t', ch3 = '\n', ch4 = '0'; //''안에는 문자 1개만 입력가능, \는 특수문자(이스케이프문자), \t는 tab(=4space), \n은 줄바꿈
+            Console.WriteLine($"{ch1}, {ch2}, {ch3}, {ch4}");
+            char ch5 = '\\'; //역슬래시 출력방법
+            Console.WriteLine($"{ch5}");
+            
+            //문자열
+            string str1 = "\tHello, \nWorld!";
+            //Console.WriteLine(str1);
+            Console.WriteLine($"{str1}");
+
+            //Boolean(bool)
+            Boolean isCorrect = false;
+            if (isCorrect) 
+            {
+                Console.WriteLine("참입니다.");
+            }
+            else 
+            {
+                Console.WriteLine("거짓입니다.");
+            }
+            Console.WriteLine(isCorrect);
+        }
+    }
+}
---
+++ chapter03/Chapter03App/VariableTestApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* int v1 = 30, v2 = 40;
+             int result = v1 + v2;
+             Console.WriteLine("결과는 " + result);*/
+            
+            //byte형
+            sbyte sbMaxVal = sbyte.MaxValue; /*sbMaxVal은 sbyte의 max값을 넣겠다는 말*/
+            sbyte sbMinVal = sbyte.MinValue;
+            Console.WriteLine($"sbyte 최소, 최대값은 {sbMinVal}, {sbMaxVal} 입니다.");
+            byte bMinVal = byte.MinValue, bMaxVal = byte.MaxValue;
+            Console.WriteLine($"byte 최소, 최대값은 {bMinVal}, {bMaxVal} 입니다.");
+
+            //short형
+            short sMinVal = short.MinValue, sMaxVal = short.MaxValue;
+            Console.WriteLine($"short 최소, 최대값은 {sMinVal}, {sMaxVal} 입니다.");
+            ushort usMinVal = ushort.MinValue, usMaxVal = ushort.MaxValue;
+            Console.WriteLine($"ushort 최소, 최대값은 {usMinVal}, {usMaxVal} 입니다.");
+
+            //int형
+            int inMinVal = int.MinValue, inMaxVal = int.MaxValue;
+            Console.WriteLine($"int 최소, 최대값은 {inMinVal}, {inMaxVal} 입니다.");
+            uint uiMinVal = uint.MinValue, uiMaxVal = uint.MaxValue;
+            Console.WriteLine($"uint 최소, 최대값은 {uiMinVal}, {uiMaxVal} 입니다.");
+
+            //long형
+            long loMinVal = long.MinValue, loMaxVal = long.MaxValue;
+            Console.WriteLine($"long 최소, 최대값은 {loMinVal}, {loMaxVal} 입니다.");
+            ulong uloMinVal = ulong.MinValue, uloMaxVal = ulong.MaxValue;
+            Console.WriteLine($"ulong 최소, 최대값은 {uloMinVal}, {uloMaxVal} 입니다.");
+
+            //float형
+            float fMinVal = float.MinValue, fMaxVal = float.MaxValue;
+            Console.WriteLine($"float 최소, 최대값은 {fMinVal}, {fMaxVal} 입니다.");
+
+            //double형
+            double dMinVal = double.MinValue, dMaxVal = double.MaxValue;
+            Console.WriteLine($"double 최소, 최대값은 {dMinVal}, {dMaxVal} 입니다.");
+
+            //decimal형
+            decimal dcMinVal = decimal.MinValue, dcMaxVal = decimal.MaxValue;
+            Console.WriteLine($"ulong 최소, 최대값은 {dcMinVal}, {dcMaxVal} 입니다.");
+
+
+
+        }
+    }
+}
