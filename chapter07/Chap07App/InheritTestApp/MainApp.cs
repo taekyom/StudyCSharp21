688a9de91a3b0911d0b697c396689fd766f0f3bb
taekyom <tkyoung1014@naver.com>
2021-02-25 12:30:45 +0900

    210225 7장 완료

---
+++ chapter07/Chap07App/InheritTestApp/MainApp.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritTestApp
+{
+    class Parent
+    {
+        protected string Name;
+
+        public Parent(string Name)
+        {
+            this.Name = Name;
+            Console.WriteLine($"{this.Name} Parent() 생성자");
+        }
+
+        ~Parent()
+        {
+            Console.WriteLine($"{this.Name} Parent() 소멸자");
+        }
+
+        public void ParentMethod()
+        {
+            Console.WriteLine($"{this.Name}.ParentMethod() 실행");
+        }
+    }
+
+    class Child : Parent
+    {
+        public string color; //색상, 클래스의 속성들을 public으로 만드는 것은 제일 위험(외부에서 접근 가능하므로)
+
+        public Child(string Name) : base(Name)
+        {
+            Console.WriteLine($"{this.Name}.child() 생성자");
+        }
+
+        /*~Child()
+        {
+            Console.WriteLine($"{this.Name} child() 소멸자");
+        }*/
+        public void ChildMethod()
+        {
+            Console.WriteLine($"{this.Name}.ChildMethod() 실행");
+        }
+    }
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Parent("부모");
+            p.ParentMethod(); //부모 클래스의 메서드를 실행
+
+            Child c = new Child("자식");
+            c.color = "황색";
+            c.ParentMethod(); //부모의 메서드 실행
+            c.ChildMethod(); //자식 클래스의 메서드를 실행
+        }
+    }
+}
---
+++ chapter07/Chap07App/OverrideTestApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverrideTestApp
+{
+    class Program
+    {
+        class ArmorSuite
+        {
+            public virtual void Initialize() //virtual : 자식 클래스에서 재정의할 것이라고 선언
+            {
+                Console.WriteLine("ArmorSuite 초기화!");
+            }
+        }
+
+        class IronMan : ArmorSuite //ironman은 armorsuite를 상속받음
+        {
+            public override void Initialize()
+            {
+                base.Initialize();
+                Console.WriteLine("리펄서 레이저 장착!"); 
+                Console.WriteLine("Micro-Rocket Launcher 장착!");
+
+            }
+        }
+        class WarMachine : ArmorSuite
+        {
+            public override void Initialize()
+            {
+                //base.Initialize(); //부모가 가진 initialize 실행 후 자식 실행(주석처리하면 부모 실행 안하고 자식 실행)
+                Console.WriteLine("ArmorSuite 개량 WarMachine 초기화!"); //base.initialize 실행 후 자식에 재정의 한 것 실행
+                Console.WriteLine("Double-Barrel 캐논 장착!");
+                Console.WriteLine("Micro-Rocket Launcher 장착!");
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ArmorSuite 생산");
+            ArmorSuite suite = new ArmorSuite();
+            suite.Initialize();
+
+            Console.WriteLine("WarMachine 생산");
+            WarMachine machine = new WarMachine();
+            machine.Initialize();
+
+            Console.WriteLine("Iron-Man 생산!");
+            IronMan ironMan = new IronMan();
+            ironMan.Initialize();
+    }
+    }
+}
