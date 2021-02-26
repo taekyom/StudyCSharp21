2f5a910c55467d4a6ca03dbfc8c02bb02ca29af1
taekyom <tkyoung1014@naver.com>
2021-02-26 17:21:25 +0900

    210225 배열 완료 커밋

---
+++ chapter10/Chapter10/2DegreeArrayApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2DegreeArrayApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 80, 74, 81, 90, 34}; //new int[5];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            Console.WriteLine("크기 변경");
+            Array.Resize(ref array, 6);
+
+            Console.WriteLine("81 index 찾기");
+            int idx = Array.IndexOf(array, 81);
+            for (int i = array.Length - 1; i >= idx;  i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[idx] = 50;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{i}번째 값 : {array[i]}\t\n");
+            }
+
+        }
+    }
+}
---
+++ chapter10/Chapter10/Chapter10/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter10
+{
+
+    class Test
+    {
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 80, 74, 81, 90, 34 }; //new int[5];
+            /*array[0] = 80;
+            array[1] = 74;
+            array[2] = 81;
+            array[3] = 90;
+            array[4] = 34;*/
+
+            Console.WriteLine($"Type of array : {array.GetType()}");
+            Console.WriteLine($"Base Type of array : {array.GetType().BaseType}");
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            /*var idx = 0;
+            foreach (var item in array) //foreach
+            {
+                Console.WriteLine($"{idx++}번째 값 : {item}");
+
+            }*/
+
+            Console.WriteLine("오름차순 정렬 후");
+            Array.Sort(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            Console.WriteLine("내림차순 정렬 후");
+            Array.Reverse(array);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+
+            int idx = Array.IndexOf(array, 34);
+            Console.WriteLine($"34의 인덱스 : {idx}");
+
+            idx = Array.IndexOf(array, 100);
+            Console.WriteLine($"100의 인덱스 : {idx}"); //결과 -1 : 배열 안에 없다는 소리
+
+            Console.WriteLine("배열 삭제");
+            Array.Clear(array, 3, 2);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"{i}번째 값 : {array[i]}");
+            }
+        }
+    }
+}
