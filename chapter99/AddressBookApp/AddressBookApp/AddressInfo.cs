a5a9f27ac54fac37f74cf145431eb79fa4d77b57
taekyom <tkyoung1014@naver.com>
2021-03-04 16:18:56 +0900

    210304 주소록 프로그램 커밋

---
+++ chapter99/AddressBookApp/AddressBookApp/AddressInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookApp
+{
+    class AddressInfo
+    {   //주소록 정보 클래스
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ chapter99/AddressBookApp/AddressBookApp/MainApp.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AddressBookApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                AddressManager manager = new AddressManager
+                {
+                    listaddress = new List<AddressInfo>()
+                };
+
+                DataFileManager fileManager = new DataFileManager();
+                manager.listaddress = fileManager.ReadData();
+
+                while (true) //무한반복
+                {
+                    Console.Clear();
+                    manager.Printmenu();
+                    int menuNum = manager.SelectMenu();
+
+                    switch (menuNum)
+                    {
+                        /*case 0: //메뉴선택 실패, 메뉴로 돌아감
+                            break;*/
+                        case 1: //주소입력 화면 전환
+                            Console.Clear();
+                            manager.InputAddress();
+                            break;
+
+                        case 2: //주소 검색
+                            Console.Clear();
+                            manager.SearchAddress();
+                            break;
+
+                        case 3: //주소 수정
+                            Console.Clear();
+                            manager.UpdateAddress();
+                            break;
+
+                        case 4: //주소 삭제
+                            Console.Clear();
+                            manager.DeleteAddress();
+                            break;
+
+                        case 5: //주소 전체출력
+                            Console.Clear();
+                            manager.PrintAllAddress();
+                            break;
+
+                        case 6: //프로그램 종료
+                            fileManager.WriteData(manager.listaddress);
+                            Environment.Exit(0);
+                            break;
+                        default: //0은 여기서 처리, 아무 로직 없음
+                            break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"예외발생 : {ex.Message}");
+            }
+        }
+    }
+}
