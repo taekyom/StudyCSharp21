98ce7d01777ea686f2459212f141efb387e8a897
taekyom <tkyoung1014@naver.com>
2021-03-04 09:25:43 +0900

    210304 18장 파일처리 추가

--- chapter18/Chap18App/TextFileApp/Program.cs
+++ chapter18/Chap18App/TextFileApp/Program.cs
@@ -11,7 +11,7 @@ namespace TextFileApp
     {
         static void Main(string[] args)
         {
-            string filePath = @"C:\Sources\GitRepository\StudyCSharp21\SampleDir\SubFolder\a.dat"; //텍스트 파일 생성 위치
+            string filePath = @"C:\Sources\GitRepository\StudyCSharp21\SampleDir\SubFolder\readme.txt"; //텍스트 파일 생성 위치
 
             StreamWriter sw = null; 
 
