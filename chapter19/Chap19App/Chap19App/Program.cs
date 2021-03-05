869b13683c971a8f1e6196414597d7e45279dfa6
taekyom <tkyoung1014@naver.com>
2021-03-05 12:41:40 +0900

    210305 19장 스레드 커밋

---
+++ chapter19/Chap19App/Chap19App/Program.cs
@@ -0,0 +1,57 @@
+﻿ using System;
+using System.Threading;
+
+namespace Chap19App
+{
+    class Program
+    {
+        static void DoSomething()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"DoSomething : {i}");
+                Thread.Sleep(10); //sleep : 시스템을 잠시 멈추는 것, 1000=1초(1ms이므로)/ 1/100초동안 멈춤
+            }
+        }
+        static void SomethingHappened()
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine($"SomethingHappened : {i}");
+                Thread.Sleep(10); //sleep : 시스템을 잠시 멈추는 것, 1000=1초(1ms이므로)/ 1/100초동안 멈춤
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(DoSomething));
+            Thread thread1 = new Thread(SomethingHappened);
+
+            try
+            {
+                Console.WriteLine("Thread 시작!");
+                thread.Start();
+                thread1.Start();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine($"Main Thread : {i}");
+                    Thread.Sleep(10); 
+
+                    if (i == 0)
+                        thread.Abort(); //abort = pause
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"예외 발생 : {ex.Message}");
+            }
+            finally
+            {
+                thread.Join();
+                thread1.Join();
+
+                Console.WriteLine($"Process End");
+            }
+        }
+    }
+}
---
+++ chapter19/Chap19App/SideTaskApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SideTaskApp
+{
+    class SideTask
+    {
+        int Count { get; set; }
+
+        public SideTask(int count)
+        {
+            this.Count = count;
+        }
+        public void CountDown()
+        {
+            try
+            {
+                while (Count > 0)
+                {
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} : {Count} 남음");
+                    Count--;
+                    Thread.Sleep(10);
+                }
+                Console.WriteLine($"Count : {Count}"); //0
+            }
+            catch(ThreadAbortException ex)
+            {
+                Console.WriteLine($"스레드 임의 종료 예외발생 : {ex.Message}");
+                //thread.resetabort();
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"스레드 예외발생 : {ex.Message}");
+            }
+            
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SideTask task = new SideTask(100);
+            Thread th = new Thread(task.CountDown);
+            th.IsBackground = false;
+            //false : 이 스레드가 종료되기 전까지는 프로세스가 끝나지 않음
+            //true : 백그라운드 스레드가 돌고 있는 상태에도 메인 스레드가 끝나면 백그라운드 스레드도 같이 종료됨
+            SideTask task2 = new SideTask(200);
+            Thread th2 = new Thread(task2.CountDown); //기본값 IsBackground false
+
+            Console.WriteLine("스레드 시작!");
+            th.Start();
+            th2.Start();
+
+            Thread.Sleep(100); //1초
+
+            Console.WriteLine("인터럽트!");
+            th2.Interrupt();
+            Console.WriteLine("임의종료");
+            //th.Abort();
+
+            Console.WriteLine("종료 대기"); //카운트 다운 중간에 표시
+            th.Join();
+            th2.Join();
+
+            Console.WriteLine("스레드/프로세스 종료!");
+
+        }
+    }
+}
